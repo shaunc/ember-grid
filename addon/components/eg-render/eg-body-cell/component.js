17c9b8d218c025bc4702754cc8ed4b9261e49bec
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2015-08-19 00:46:29 +0100

    Alternate colours for body rows

--- addon/components/eg-render/eg-body-cell/component.js
+++ addon/components/eg-render/eg-body-cell/component.js
@@ -5,9 +5,13 @@ import layout from './template';
 export default Ember.Component.extend({
   layout: layout,
   classNames: ['cell', 'eg-body-cell'],
+  classNameBindings: ['isOddRow:odd'],
   attributeBindings: ['style', 'rowIndex:data-row-index'],
 
   width: Ember.computed.alias('column.width'),
+  isOddRow: Ember.computed('rowIndex', function() {
+    return this.get('rowIndex') % 2 === 1;
+  }),
 
   style: Ember.computed('column.offset', 'width', function(){
     var {'column.offset': offset, width, rowHeight} = this.getProperties(
