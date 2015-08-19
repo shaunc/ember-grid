688d4e8ba5297c2cc1084f1d54d130ed3019a598
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2015-08-19 09:33:25 +0100

    Tidy up code

--- addon/components/eg-header/component.js
+++ addon/components/eg-header/component.js
@@ -11,12 +11,12 @@ export default Ember.Component.extend({
   willInsertElement: function() {
     this._super.apply(this, arguments);
 
-	    var parentView = this.get('parentView');
-	    if (parentView instanceof EmberGridColumn) {
-	    	parentView.set('_column._zones.header', this.attrs);
-	    	parentView.set('_column._zones.header.element', this.get('element'));
-	    }
-		  this.get('element').style.display = 'none';
+    var parentView = this.get('parentView');
+    if (parentView instanceof EmberGridColumn) {
+    	parentView.set('_column._zones.header', this.attrs);
+    	parentView.set('_column._zones.header.element', this.get('element'));
+    }
+	  this.get('element').style.display = 'none';
 	}
 
 });
