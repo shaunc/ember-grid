7effefb337b2b166aa586c1f5deec01fe3e3e37b
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2015-08-22 14:26:46 +0100

    fix body cell fields when field is a function. Demonstrate accessor functions in dummy app

--- tests/dummy/app/application/controller.js
+++ tests/dummy/app/application/controller.js
@@ -8,8 +8,17 @@ export default Ember.Controller.extend({
   gridRows: generateData(500, {
     name: 'name', 
     age: {name: 'age', options: {type: 'adult'}},
-    salary: {name: 'dollar', options: {max: 200000}},
+    salary: {name: 'floating', options: {min:0, max: 200000, fixed:2}},
     email: {name: 'email', options: {domain: 'example.com'}}
-    }, 4359)
+    }, 4359),
+
+  dollarSalary: function(row) {
+  	return '$'+row.salary.toFixed(2);
+  },
+
+  averageSalary: Ember.computed('gridRows.@each.salary', function() {
+  	return this.averag
+  }),
+  averageAge: Ember.computed('gridRows.@each.age')
 
 });
