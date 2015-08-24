849f3559c8d7680959a87bcc8450c4f182abaf3b
Shaun Cutts <shaunc@factfiber.com>
2015-08-23 22:50:04 -0400

    fixes init; adds columns specified as string

---
+++ tests/dummy/app/grid-example/component.js
@@ -0,0 +1,24 @@
+// dummy/grid-example
+
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  layout: Ember.computed('layoutString', 'options', function(){
+    return Ember.Handlebars.compile(this.get('layoutString'))
+  }),
+  didReceiveAttrs() {
+    this._super();
+
+    var options = this.getAttr('options');
+    for (let opt in options) {
+      this.set(opt, options[opt]);
+    }
+    var templateString = this.getAttr('layoutString');
+    if (templateString.string != null) {
+      templateString = templateString.string;
+    }
+    this.set('layoutString', templateString);
+  }
+})
+
+
