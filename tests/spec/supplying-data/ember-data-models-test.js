eb8e0e582cca4c443c5fda2ebd1050db92b52b1d
Bryan Crotaz <bryan.crotaz@silvercurve.co.uk>
2015-09-11 14:48:35 +0100

    Added test for issue #24

--- tests/spec/supplying-data/ember-data-models-test.js
+++ tests/spec/supplying-data/ember-data-models-test.js
@@ -2,6 +2,7 @@ import { test } from '../../helpers/chai-assert';
 import moduleForIntegration from '../../helpers/test-component';
 import { columnsWithWidths } from '../../helpers/example-data';
 import { renderTemplate } from '../../helpers/render';
+import Ember from 'ember';
 
 moduleForIntegration("spec/supplying-data/ember-data-models-test",
 					 "Data Source | supply data using ember-data models");
@@ -33,7 +34,22 @@ function getDummyItems(count) {
 	return data;
 }
 
-test('column renders field by key when data is one ember-data model', function() {
+function getDummyItemsAsEmberData(count) {
+	var data = Ember.A();
+
+	for (var i = 0; i < count; i++)
+	{
+		data.pushObject(getDummyItem());
+	}
+
+	return {
+		toArray: function() {
+			return data;
+		}
+	};
+}
+
+test('column renders field by key when data is plain array of one ember-data model', function() {
 	var width = 500;
 	var height = 200;
 	var columns = columnsWithWidths;
@@ -43,11 +59,36 @@ test('column renders field by key when data is one ember-data model', function()
 	expectElement(".row", 1, {contains: "name"});
 });
 
-test('column renders field by field name when data is one ember-data model', function() {
+test('column renders field by field name when data is plain array of one ember-data model', function() {
 	var width = 500;
 	var height = 200;
 	var columns = columnsWithWidths;
 	var data = getDummyItems(1);
+
+	// specify a field on the column which overrides key
+	columns.forEach(col => {
+		col.field = col.key;
+	});
+
+	renderTemplate(this, {width, height, data, columns});
+	expectElement(".row", 1, {contains: "name"});
+});
+
+test('column renders field by key when data is ember-data array of model', function() {
+	var width = 500;
+	var height = 200;
+	var columns = columnsWithWidths;
+	var data = getDummyItemsAsEmberData(1);
+
+	renderTemplate(this, {width, height, data, columns});
+	expectElement(".row", 1, {contains: "name"});
+});
+
+test('column renders field by field name when data is ember-data array of model', function() {
+	var width = 500;
+	var height = 200;
+	var columns = columnsWithWidths;
+	var data = getDummyItemsAsEmberData(1);
 
 	// specify a field on the column which overrides key
 	columns.forEach(col => {
